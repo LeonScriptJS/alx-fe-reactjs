0cdc434b7610d61dff9e88db907af9abc93e9f08
DonLeonJnr <leonardudegbulem@gmail.com>
2026-01-14 23:36:38 +0100

    Create Specific Components in a React Application_001

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   
-     <WelcomeMessage />
-    
+    <>
+      <Header />
+
+      <MainContent />
+
+      <Footer />
+    </>
   );
 }
 
-export default App
+export default App;
