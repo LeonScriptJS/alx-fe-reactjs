caaa9ff22d7bf5d49415c692f939ea9d1e06882a
LeonScriptJS <leonardudegbulem@gmail.com>
2026-02-01 17:07:23 +0100

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,12 @@
 import react from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
     <>
       <div>
         <h1>Github User Search</h1>
+        <Search />
       </div>
     </>
   );
