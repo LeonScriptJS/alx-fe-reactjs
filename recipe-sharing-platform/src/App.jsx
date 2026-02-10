50eefb53d8e4469cdd0a373f41587898dfea7c65
LeonScriptJS <leonardudegbulem@gmail.com>
2026-02-10 11:11:42 +0100

    Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import HomePage from './components/HomePage';
 
 
 function App() {
 
   return (
     <>
-    
+    <HomePage />
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import recipes from "../data.json/";
+
+const HomePage = () => {
+  const [recipeList, setRecipeList] = useState([]);
+  useEffect(() => {
+    setRecipeList(recipes);
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        Recipe Sharing Platform
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {recipeList.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-x1 transition"
+          >
+            <img
+              src="{recipe.image}"
+              alt="{recipe.title}"
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-lg font-semibold mb-2">{recipe.title}</h2>
+              <p className="text-gray-600 text-sm">{recipe.summary}</p>
+            
+             <a
+                href="#"
+                className="text-blue-600 text-sm font-medium hover:underline"
+              >
+                View Recipe →
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
