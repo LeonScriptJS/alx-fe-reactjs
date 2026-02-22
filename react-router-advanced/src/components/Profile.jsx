0009273d9fec71382d2a5a561ffd90a47187efa8
LeonScriptJS <leonardudegbulem@gmail.com>
2026-02-22 23:09:54 +0100

    Advanced Routing in React with React Router_004

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,6 +1,17 @@
-import { Link, Outlet } from "react-router-dom";
 import React from "react";
+import { Link, Outlet, Routes, Route } from "react-router-dom";
+import ProfileDetails from "../pages/ProfileDetails";
+import ProfileSettings from "../pages/ProfileSettings";
 
+/*
+Routes
+Route
+ProfileDetails
+ProfileSettings
+Dynamic routing implemented
+Protected route component created
+Protected route implemented
+*/
 
 function Profile() {
   return (
@@ -8,9 +19,15 @@ function Profile() {
       <h2>Profile Page</h2>
 
       <nav>
-        <Link to="details">Details</Link> |{" "}
-        <Link to="settings">Settings</Link>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
       </nav>
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
 
       <Outlet />
     </div>
