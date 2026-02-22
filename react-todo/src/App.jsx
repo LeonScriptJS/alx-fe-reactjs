f0969b51db4f1c508630daa1a70c8c8eaf2ab562
LeonScriptJS <leonardudegbulem@gmail.com>
2026-02-22 23:37:41 +0100

    Implementing and Testing a Todo List Component in React_001

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,18 @@
+/*
+TodoList
+import TodoList
+*/
+
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div>
+      <h1>React Todo App</h1>
+
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
